873b7184661f731ff34eed90fd35f21c7cd7ea2e
kripamariamshaji <kripamariamshaji@gmail.com>
2022-03-16 10:48:30 -0400

    manager_commit_kripa

--- app/views.py
+++ app/views.py
@@ -14,6 +14,112 @@ def index(request):
 def manager_Dashboard(request):
     return render(request,'manager\manager_Dashboard.html')
 
+def manager_dept(request):
+    return render(request,'manager\manager_dept.html') 
+def manager_emp_dept(request):
+    return render(request,'manager\manager_emp_dept.html')   
+def manager_projects(request):
+    return render(request,'manager\manager_projects.html')
+def manager_proj_list(request):
+    return render(request,'manager\manager_proj_list.html')
+def manager_proj_details(request):
+    return render(request,'manager\manager_proj_details.html')
+def manager_proj_manager(request):
+    return render(request,'manager\manager_proj_manager.html')
+def manager_proj_manager1(request):
+    return render(request,'manager\manager_proj_manager1.html')
+def manager_proj_manager2(request):
+    return render(request,'manager\manager_proj_manager2.html')
+
+def manager_completed_proj(request):
+    return render(request,'manager\manager_completed_proj.html')
+def manager_completed_proj_details(request):
+    return render (request,'manager\manager_completed_proj_details.html')
+def manager_completed_proj_manager(request):
+    return render(request, 'manager\manager_completed_proj_manager.html')
+def manager_completed_proj_manager_list(request):
+    return render(request,'manager\manager_completed_proj_manager_list.html')
+def manager_completed_proj_manager_tl(request):
+    return render(request,'manager\manager_completed_proj_manager_tl.html')
+def manager_emp_dept_list(request):
+    return render(request,'manager\manager_emp_dept_list.html')
+def manager_emp_dept_details(request):
+    return render(request,'manager\manager_emp_dept_details.html')
+def manager_emp_profile(request):
+    return render(request,'manager\manager_emp_profile.html')
+def manager_emp_involved_projects(request):
+    return render(request,'manager\manager_emp_involved_projects.html')
+def manager_emp_attendance(request):
+    return render(request,'manager\manager_emp_attendance.html')
+
+def manager_emp_attendance_show(request):
+    return render(request,'manager\manager_emp_attendance_show.html')
+
+
+def manager_upcoming(request):
+    return render(request,'manager\manager_upcoming.html')
+
+def manager_createproject(request):
+    return render(request,'manager\manager_createproject.html')
+
+def manager_upcoming_project(request):
+    return render(request,'manager\manager_upcoming_project.html')
+
+def manager_give_project(request):
+    return render(request,'manager\manager_give_project.html')
+
+def manager_accepted_project(request):
+    return render(request,'manager\manager_accepted_project.html')
+
+def manager_rejected_project(request):
+    return render(request,'manager\manager_rejected_project.html')
+
+def manager_task(request):
+    return render(request,'manager\manager_task.html')
+
+def manager_givetask(request):
+    return render(request,'manager\manager_givetask.html')
+
+def manager_current_task(request):
+    return render(request,'manager\manager_current_task.html')
+
+def manager_previous_task(request):
+    return render(request,'manager\manager_previous_task.html')
+
+def manager_registration_details(request):
+    return render(request,'manager\manager_registration_details.html')  
+
+def manager_new_department(request):
+    return render(request,'manager\manager_new_department.html') 
+
+def manager_create_department(request):
+    return render(request,'manager\manager_create_department.html')
+
+def manager_attendance(request):
+    return render(request,'manager\manager_attendance.html') 
+
+def manager_attendance_show(request):
+    return render(request,'manager\manager_attendance_show.html')
+
+def manager_reported_issues(request):
+    return render(request,'manager\manager_reported_issues.html') 
+
+def manager_issues(request):
+    return render(request,'manager\manager_issues.html')
+
+def manager_issues_form(request):
+    return render(request,'manager\manager_issues_form.html')
+
+def manager_myissues(request):
+    return render(request,'manager\manager_myissues.html')
+
+
+def manager_reported_detail(request):
+    return render(request,'manager\manager_reported_detail.html')
+
+def manager_reported_issue_show(request):
+    return render(request,'manager\manager_reported_issue_show.html')
+
 
 
     
--- software/urls.py
+++ software/urls.py
@@ -1,16 +1,59 @@
-from django.contrib import admin
+
 from django.urls import path
 from app import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    
     path('index', views.index, name='index'),
 
 
-    
+
 
     #***************************** Manager ******************************
 
-    path('manager_Dashboard',views.manager_Dashboard),
+    path('manager_Dashboard',views.manager_Dashboard), 
+    path('manager_dept',views.manager_dept),
+    path('manager_emp_dept',views.manager_emp_dept),  
+    path('manager_projects',views.manager_projects), 
+    path('manager_proj_list', views.manager_proj_list),
+    path('manager_proj_details', views.manager_proj_details),
+    path('manager_proj_manager', views.manager_proj_manager),
+    path('manager_proj_manager1', views.manager_proj_manager1),
+    path('manager_proj_manager2', views.manager_proj_manager2),
+    path('manager_completed_proj', views.manager_completed_proj),
+    path('manager_completed_proj_details', views.manager_completed_proj_details),
+    path('manager_completed_proj_manager', views.manager_completed_proj_manager),
+    path('manager_completed_proj_manager_list', views.manager_completed_proj_manager_list),
+    path('manager_completed_proj_manager_tl', views.manager_completed_proj_manager_tl),
+    path('manager_emp_dept_list', views.manager_emp_dept_list),
+    path('manager_emp_dept_details', views.manager_emp_dept_details),
+    path('manager_emp_profile', views.manager_emp_profile),
+    path('manager_emp_involved_projects', views.manager_emp_involved_projects),
+    path('manager_emp_attendance', views.manager_emp_attendance),
+    path('manager_emp_attendance_show',views.manager_emp_attendance_show),
+
+    path('manager_upcoming',views.manager_upcoming),
+    path('manager_createproject',views.manager_createproject), 
+    path('manager_upcoming_project',views.manager_upcoming_project),
+    path('manager_give_project',views.manager_give_project),
+    path('manager_accepted_project',views.manager_accepted_project),
+    path('manager_rejected_project',views.manager_rejected_project),
+    path('manager_task',views.manager_task),
+    path('manager_givetask',views.manager_givetask),
+    path('manager_current_task',views.manager_current_task),
+    path('manager_previous_task',views.manager_previous_task),
+    path('manager_registration_details',views.manager_registration_details), 
+    path('manager_new_department',views.manager_new_department),
+    path('manager_create_department',views.manager_create_department),
+    path('manager_attendance',views.manager_attendance), 
+    path('manager_attendance_show',views.manager_attendance_show),
+    path('manager_reported_issues',views.manager_reported_issues),
+    path('manager_issues',views.manager_issues),
+    path('manager_issues_form',views.manager_issues_form),
+    path('manager_myissues',views.manager_myissues),
+    path('manager_reported_detail',views.manager_reported_detail),
+    path('manager_reported_issue_show',views.manager_reported_issue_show),
+
+    
     
 ]
